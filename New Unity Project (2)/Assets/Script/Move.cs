196d4c4e884a63fef0a0894f967c49073ffe4711
whwkd11010 <43107764+whwkd11010@users.noreply.github.com>
2019-11-18 14:42:04 +0900

    Add git ignore
    git ignore added

---
+++ New Unity Project (2)/Assets/Script/Move.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 20;
+    Vector3 lookDirection;
+
+
+    void Update()
+    {
+        moveObject();
+    }
+
+    void moveObject()
+    {
+
+
+        float xx = Input.GetAxisRaw("Vertical");
+        float zz = Input.GetAxisRaw("Horizontal");
+        lookDirection = xx * Vector3.forward + zz * Vector3.right;
+
+
+       transform.rotation = Quaternion.LookRotation(lookDirection);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.LookRotation(lookDirection);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.LookRotation(lookDirection);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.rotation = Quaternion.LookRotation(lookDirection);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.LookRotation(lookDirection);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+       
+    }
+
+}
---
+++ New Unity Project (2)/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public int speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveObject();
+        
+
+    }
+
+    void moveObject()
+    {
+        /*
+        float keyHorizontal = Input.GetAxis("Horizontal");
+
+        float keyVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * speed * Time.smoothDeltaTime * keyHorizontal, Space.World);
+
+        transform.Translate(Vector3.up * speed * Time.smoothDeltaTime * keyVertical, Space.World);*/
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+}
