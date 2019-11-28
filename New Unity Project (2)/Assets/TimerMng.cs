91d4784d193021c7dfb08fe35d57ceb54caad3ec
sungjae <alstjdwo1601@naver.com>
2019-11-28 22:38:20 +0900

    Timer & Camera
    add Timer and Camera perspective

---
+++ New Unity Project (2)/Assets/TimerMng.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerMng : MonoBehaviour
+{
+
+    private static float time;
+
+    void Start()
+    {
+        time = 0.00f;
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        float t = Mathf.Abs(time);
+        Text uiText = GetComponent<Text>();
+        uiText.text = "Time :  " + t.ToString();
+    }
+    
+}
---
+++ New Unity Project (2)/Assets/time.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class time : MonoBehaviour
+{
+
+    private static float time1;
+    
+    void Start()
+    {
+        time1 = 0.00f;
+    }
+
+    private void Update()
+    {
+        time1 += Time.deltaTime;
+        
+
+        float t = Mathf.Abs(time1);
+        Text uiText = GetComponent<Text>();
+        uiText.text = string.Format("{0:N2}", time1);
+        uiText.text = "Time : " + t.ToString();
+    }
+
+
+    
+}
